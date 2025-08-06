a7d5aee17d9e3b566ce29c08ac35b1e7f4f6ee25
Micaela Rapisardi <rapisardimicaela.data@gmail.com>
2025-08-06 10:39:10 -0300

    usuarios y contraseñas

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// listar novedades
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'B&DM Abogados' });
+  res.render('admin/novedades',{
+    layout:'admin/layout',
+    persona:req.session.nombre
+  });
 });
 
 module.exports = router;
